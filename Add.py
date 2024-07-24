2cb3a826d0f85f2d83edabf0764feb79880c83e0
AMIRUDDIN2104 <muhammadamiruddinmuhamad@gmail.com>
2024-07-24 22:20:33 +0800

    Add

--- Add.py
+++ Add.py
@@ -2,7 +2,6 @@
 #Add
 #Subtract
 #Divide
-
 def add(a, b):
         return a + b
 
@@ -17,18 +16,21 @@ print("1. Add")
 
 while True:
 
-    choice = input ("Enter choice (1/2/3): ")
+    choice = input ("Choose the option (1/2/3): ")
 
     if choice in ('1', '2', '3'):
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
 
+#addition
     if choice == '1':
         print(num1, "+", num2, "=",add(num1, num2))
 
+#subtraction
     elif choice == '2':
          print(num1, "-", num2, "=",subtract(num1, num2))
 
+#division
     elif choice == '3':
         print(num1, "/", num2, "=",divide(num1, num2))
 
