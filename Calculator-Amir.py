8b6ea48b1935aa0988ded1e883440e796c10b927
AMIRUDDIN2104 <muhammadamiruddinmuhamad@gmail.com>
2024-07-24 22:03:04 +0800

    UpdateCodeForModulus

--- Calculator-Amir.py
+++ Calculator-Amir.py
@@ -7,7 +7,15 @@ import math
 def multiply(num1, num2):
     return num1 * num2
 
+def calculate_modulus(dividend, divisor):
+    return dividend % divisor
+
 #multiplication
 num1 = float(input("Enter Number1 for multiplication: "))
 num2 = float(input("Enter Number2 for multiplication: "))
 print(num1 * num2)
+
+#modulus
+dividend = float(input("\nEnter dividend number: "))
+divisor = float(input("Enter divisor: "))
+print(dividend % divisor)
