072222d7f27cb24504399b36202ccf9d191d9594
AMIRUDDIN2104 <muhammadamiruddinmuhamad@gmail.com>
2024-07-24 22:43:30 +0800

    UpdateCodeExponent

--- Calculator-Amir.py
+++ Calculator-Amir.py
@@ -13,6 +13,9 @@ def calculate_modulus(dividend, divisor):
 def calculate_logarithm(base, number):
     return math.log(number, base)
 
+def calculate_exponent(base, exponent):
+    return base **
+
 #multiplication
 num1 = float(input("Enter Number1 for multiplication: "))
 num2 = float(input("Enter Number2 for multiplication: "))
@@ -27,3 +30,8 @@ print(dividend % divisor)
 base = float(input("\nEnter base: "))
 number = float(input("Enter number (log X): "))
 print(math.log(number, base))
+
+#exponent
+base_exp = float(input("\nEnter base exponent: "))
+exponent = float(input("Enter exponent number: "))
+print(base_exp ** exponent)
