33d148221c5dbb8ff1bc3175b82271b970b9fd51
AMIRUDDIN2104 <muhammadamiruddinmuhamad@gmail.com>
2024-07-24 16:02:31 +0800

    multiplication code by amir

---
+++ Calculator-Amir.py
@@ -0,0 +1,13 @@
+# python code by amir
+# calculator
+# multiply/modulus/logs/exponential
+
+import math
+
+def multiply(num1, num2):
+    return num1 * num2
+
+#multiplication
+num1 = float(input("Enter Number1 for multiplication: "))
+num2 = float(input("Enter Number2 for multiplication: "))
+print(num1 * num2)
