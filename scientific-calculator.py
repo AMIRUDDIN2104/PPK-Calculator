f209a9b88a0fbb291e79bd1b55a3c3cf27e78ee2
Adha Rizwan <B022110131@STUDENT.UTEM.EDU.MY>
2024-07-24 22:37:11 +0800

    scientific-calculator v2.1

--- scientific-calculator.py
+++ scientific-calculator.py
@@ -213,7 +213,7 @@ btnsq = Button(calc, text="\u221A", width=6,height=2, bg='honeydew',font=('Couri
 btnAdd = Button(calc, text="+", width=6,height=2, bg='honeydew',font=('Courier New', 15, 'bold'),bd=4, command=lambda: added_value.operation("add")).grid(row=1, column=3, pady=0.5)
 btnSub = Button(calc, text="-", width=6,height=2, bg='honeydew',font=('Courier New', 15, 'bold'),bd=4,command=lambda: added_value.operation("sub")).grid(row=2, column=3, pady=0.5)
 btnMul = Button(calc, text="x", width=6, height=2,bg='honeydew',font=('Courier New', 15, 'bold'),bd=4, command=lambda: added_value.operation("multi")).grid(row=3, column=3, pady=0.5)
-btnDiv = Button(calc, text="/", width=6,height=2, bg='honeydew',font=('Courier New', 15, 'bold'),bd=4, command=lambda: added_value.operation("divide")).grid(row=4, column=3, pady=0.5)
+btnDiv = Button(calc, text="÷", width=6,height=2, bg='honeydew',font=('Courier New', 15, 'bold'),bd=4, command=lambda: added_value.operation("divide")).grid(row=4, column=3, pady=0.5)
 btnZero = Button(calc, text="0", width=6,height=2, bg='lightyellow', fg='black',font=('Courier New', 15, 'bold'),bd=4, command=lambda: added_value.numberEnter(0)).grid(row=5, column=1, pady=0.5)
 btnDot = Button(calc, text=".", width=6,height=2, bg='honeydew',font=('Courier New', 15, 'bold'),bd=4, command=lambda: added_value.numberEnter(".")).grid(row=5, column=0, pady=0.5)
 btnPM = Button(calc, text=chr(177), width=6,height=2, bg='honeydew',font=('Courier New', 15, 'bold'),bd=4, command=added_value.mathPM).grid(row=5, column=2, pady=0.5)
